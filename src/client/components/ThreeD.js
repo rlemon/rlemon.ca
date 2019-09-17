f92d849d5d129976b133009e47fa63757d913d79
rlemon <rob.lemon@gmail.com>
2019-09-17 10:32:20 -0400

    removing three stuff, it doesnt work

--- src/client/components/ThreeD.js
+++ src/client/components/ThreeD.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import STLViewer from './Three/ThreeViewer';
 
 export default class ThreeD extends React.Component {
 
     render() {
         return (
             <>
-                <STLViewer
-                    url='assets/stls/Lipless Crankbait.stl'
-                    width={500}
-                    height={500}
-                    modelColor='#cc4444'
-                    backgroundColor='#242424'
-                    rotate
-                    orbitControls
-                />
+                3d viewer
             </>
         );
     }
